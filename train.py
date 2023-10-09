2e0dcb043cf2ecfd1e46402003e9f185a5095644
hwzhou2020 <hzhou7@caltech.edu>
2023-10-09 00:36:21 +0000

    lr_scheduler to the end of every epoch

--- train.py
+++ train.py
@@ -40,8 +40,6 @@ if __name__ == '__main__':
         iter_data_time = time.time()    # timer for data loading per iteration
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
         visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
-        model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
-        model.update_learning_rate()    # update learning rates in the beginning of every epoch.
         for i, data in enumerate(dataset):  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
             if total_iters % opt.print_freq == 0:
@@ -75,4 +73,5 @@ if __name__ == '__main__':
             model.save_networks('latest')
             model.save_networks(epoch)
 
+        model.update_learning_rate()    # update learning rates in the end of every epoch.
         print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
