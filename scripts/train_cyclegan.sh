079561b4c82a7e3f744892ee01246af61d8f3f86
hwzhou2020 <hzhou7@caltech.edu>
2023-10-09 00:09:26 +0000

    test run for multi-gpu

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --pool_size 50 --no_dropout
+python train.py --gpu_ids 0,1,2,3 --batch_size 16 --norm instance --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --pool_size 50 --no_dropout
