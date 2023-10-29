e4a369042a0e104e83de8f69ef4cb776f6dc272f
hwzhou2020 <hzhou7@caltech.edu>
2023-10-28 21:34:01 -0700

    Trial pass

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --gpu_ids 0,1,2,3 --batch_size 16 --norm instance --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --pool_size 50 --no_dropout
+python train.py --gpu_ids 0,1,2,3 --batch_size 32 --norm instance --dataroot ./datasets/Phase2HE --name PhaseHE_cyclegan --model cycle_gan --pool_size 50 --no_dropout --n_epochs 80 --n_epochs_decay 100
