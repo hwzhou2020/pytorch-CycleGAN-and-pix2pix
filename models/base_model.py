12eb45de9d7bde6241d5e0d12b07a95e02160f98
hwzhou2020 <hzhou7@caltech.edu>
2023-12-02 16:09:41 -0800

    Add testing part of ua cyclegan

--- models/base_model.py
+++ models/base_model.py
@@ -85,7 +85,7 @@ class BaseModel(ABC):
             self.schedulers = [networks.get_scheduler(optimizer, opt) for optimizer in self.optimizers]
         if not self.isTrain or opt.continue_train:
             load_suffix = 'iter_%d' % opt.load_iter if opt.load_iter > 0 else opt.epoch
-            self.load_networks(load_suffix)
+            self.load_networks(load_suffix, opt.model)
         self.print_networks(opt.verbose)
 
     def eval(self):
@@ -173,30 +173,67 @@ class BaseModel(ABC):
         else:
             self.__patch_instance_norm_state_dict(state_dict, getattr(module, key), keys, i + 1)
 
-    def load_networks(self, epoch):
+    def load_networks(self, epoch, model_type):
         """Load all the networks from the disk.
 
         Parameters:
             epoch (int) -- current epoch; used in the file name '%s_net_%s.pth' % (epoch, name)
         """
-        for name in self.model_names:
-            if isinstance(name, str):
-                load_filename = '%s_net_%s.pth' % (epoch, name)
-                load_path = os.path.join(self.save_dir, load_filename)
-                net = getattr(self, 'net' + name)
-                if isinstance(net, torch.nn.DataParallel):
-                    net = net.module
-                print('loading the model from %s' % load_path)
-                # if you are using PyTorch newer than 0.4 (e.g., built from
-                # GitHub source), you can remove str() on self.device
-                state_dict = torch.load(load_path, map_location=str(self.device))
-                if hasattr(state_dict, '_metadata'):
-                    del state_dict._metadata
+        if model_type == 'ua_cycle_gan':
+            for name in self.model_names:
+                if isinstance(name, str):
+                    load_filename = '%s_net_%s.pth' % (epoch, name)
+                    load_path = os.path.join(self.save_dir, load_filename)
+                    net = getattr(self, 'net' + name)
+                    if isinstance(net, torch.nn.DataParallel):
+                        net = net.module
+                    print('loading the model from %s' % load_path)
+                    # if you are using PyTorch newer than 0.4 (e.g., built from
+                    # GitHub source), you can remove str() on self.device
+                    state_dict_ua = torch.load(load_path, map_location=str(self.device))
+                    if hasattr(state_dict_ua, '_metadata'):
+                        del state_dict_ua._metadata
 
-                # patch InstanceNorm checkpoints prior to 0.4
-                for key in list(state_dict.keys()):  # need to copy keys here because we mutate in loop
-                    self.__patch_instance_norm_state_dict(state_dict, net, key.split('.'))
-                net.load_state_dict(state_dict)
+                    # # patch InstanceNorm checkpoints prior to 0.4
+                    # for key in list(state_dict_ua.keys()):  # need to copy keys here because we mutate in loop
+                    #     print(key)
+
+                    # print(state_dict_ua.keys())
+                    net_dict = net.state_dict()
+                    # print(net_dict.keys())
+                    filtered_Init_netG_A_dict = {k: v for k, v in state_dict_ua.items() if k in net_dict}
+                    for k, v in filtered_Init_netG_A_dict.items():
+                        net_dict[k] = state_dict_ua[k]
+                    for k, v in state_dict_ua.items():
+                        k1 = k
+                        k_parse = k.split('.')
+                        if k_parse[-3] == 'conv_block' and int(k_parse[-2]) == 6:
+                            k_parse[-2] = '5'
+                            k = '.'.join(k_parse)
+                            net_dict[k] = state_dict_ua[k1]
+                            
+
+                    net_dict.update(filtered_Init_netG_A_dict)
+                    net.load_state_dict(net_dict)
+        else:
+            for name in self.model_names:
+                if isinstance(name, str):
+                    load_filename = '%s_net_%s.pth' % (epoch, name)
+                    load_path = os.path.join(self.save_dir, load_filename)
+                    net = getattr(self, 'net' + name)
+                    if isinstance(net, torch.nn.DataParallel):
+                        net = net.module
+                    print('loading the model from %s' % load_path)
+                    # if you are using PyTorch newer than 0.4 (e.g., built from
+                    # GitHub source), you can remove str() on self.device
+                    state_dict = torch.load(load_path, map_location=str(self.device))
+                    if hasattr(state_dict, '_metadata'):
+                        del state_dict._metadata
+
+                    # patch InstanceNorm checkpoints prior to 0.4
+                    for key in list(state_dict.keys()):  # need to copy keys here because we mutate in loop
+                        self.__patch_instance_norm_state_dict(state_dict, net, key.split('.'))
+                    net.load_state_dict(state_dict)
 
     def print_networks(self, verbose):
         """Print the total number of parameters in the network and (if verbose) network architecture
